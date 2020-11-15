bdd91013255a54ee51d1a1f1d0e70337edf6b40c
Enes Demirag <ensdmrg@gmail.com>
2020-11-15 21:49:27 +0300

    add unpickle operation

---
+++ unpickle.py
@@ -0,0 +1,56 @@
+# The archive contains the files data_batch_1, data_batch_2, ... as well as test_batch.
+# Each of these files is a Python "pickled" object produced with cPickle.
+# Each file has 10000 serialized images with labels.
+# b'batch_label', b'labels', b'data', b'filenames'.
+
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+classes = {
+    0: "airplane",
+    1: "automobile",
+    2: "bird",
+    3: "cat",
+    4: "deer",
+    5: "dog",
+    6: "frog",
+    7: "horse",
+    8: "ship",
+    9: "truck",
+}
+
+
+def unpickle(file):
+    """
+    file: path of the pickled file
+    return: dictionary object
+    """
+    with open(file, "rb") as fo:
+        dict = pickle.load(fo, encoding="bytes")
+    return dict
+
+
+def unserialize(data):
+    """
+    data: (N, 3072) array of serialized pixel data
+    return: (N, 32, 32, 3) tensor as
+    """
+    N = data.shape[0]
+    tensor = data.reshape(N, 3, 32, 32).transpose(0, 2, 3, 1)
+    return tensor
+
+
+dataset = unpickle("cifar10/data_batch_1")
+
+data = dataset[b"data"]
+labels = dataset[b"labels"]
+images = unserialize(data)
+
+index = np.random.randint(10000)
+sample_img = images[index]
+sample_label = classes[labels[index]]
+
+plt.imshow(sample_img)
+plt.title(sample_label)
+plt.show()
