cef11a45dfd998f8ac23330e7b7839377f5f6fe5
Enes Demirag <ensdmrg@gmail.com>
2020-11-16 16:05:29 +0300

    implement one hot encoding

--- preprocessing.py
+++ preprocessing.py
@@ -1,41 +1,49 @@
 from unpickle import *
 from tensorflow.keras.datasets import cifar10
-from sklearn.preprocessing import OneHotEncoder
+
 
 def prepare_pixels(data):
     """
     Convert data from integers between 0 - 255 to floats between 0 - 1
     """
-    data = data.astype('float32')
+    data = data.astype("float32")
     norm = data / 255.0
     return norm
 
+
 def prepare_labels(labels):
-    one_hot_encoder = OneHotEncoder(sparse=False)
-    one_hot_encoder.fit(labels)
-    labels = one_hot_encoder.transform(labels)
-    return labels
+    """
+    One Hot Encoding
+    """
+    labels = np.array(labels)
+    N = labels.shape[0]
+    one_hot = np.zeros((N, 10))
+
+    for index, value in enumerate(labels):
+        one_hot[index][value] = 1
+
+    return one_hot
 
 
 def get_train_data(batch_no):
-    train_set = unpickle('cifar10/data_batch_' + str(batch_no))
-    train_data = train_set[b'data']
+    train_set = unpickle("cifar10/data_batch_" + str(batch_no))
+    train_data = train_set[b"data"]
     train_images = unserialize(train_data)
-    labels = train_set[b'labels']
-
+    labels = train_set[b"labels"]
     images = prepare_pixels(train_images)
     labels = prepare_labels(labels)
     return images, labels
 
+
 def get_test_data():
-    test_set = unpickle('cifar10/test_batch')
-    test_data = test_set[b'data']
+    test_set = unpickle("cifar10/test_batch")
+    test_data = test_set[b"data"]
     test_images = unserialize(test_data)
-    labels = test_set[b'labels']
-    
+    labels = test_set[b"labels"]
     images = prepare_pixels(test_images)
     labels = prepare_labels(labels)
     return images, labels
+
 
 def get_data_from_tensorflow():
     (images_train, labels_train), (images_test, labels_test) = cifar10.load_data()
