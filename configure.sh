b86593ab5cc8a1dc11a63b5e4a65bc596c943152
Enes Demirag <ensdmrg@gmail.com>
2020-11-17 14:41:03 +0300

    Update configuration file

--- configure.sh
+++ configure.sh
@@ -1,3 +1,8 @@
+# Dependencies
+pip3 install numpy matplotlib tensorflow pandas pillow
+
+# Dataset
 wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 tar -xzf cifar-10-python.tar.gz
 mv cifar-10-batches-py cifar
+rm -rf cifar-10-python.tar.gz
